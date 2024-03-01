20649276acbee1db332c92d3833426c5a5094478
codadevv <colenolson@gmail.com>
2024-02-29 17:09:06 -0800

    cleaned up the controller removed junk

--- stockbuysellbackendapi/stockbuysellbackendapi/Controllers/StockDataController.cs
+++ stockbuysellbackendapi/stockbuysellbackendapi/Controllers/StockDataController.cs
@@ -4,13 +4,12 @@ using System.Net.Http;
 namespace AccountOwnerServer.Controllers
 {
     [ApiController]
-
     /*This is the master controller route aka the source to acess this controller if a client types in api/Myass they will get the stockdatacontorller and then we can select what method action we want*/
-    [Route("api/[controller]")]
+    [Route("api/stockdata")]
     public class StockDataController : ControllerBase
     {
-        private static HttpClient _httpClient;
 
+        private static HttpClient _httpClient;
         public StockDataController()
         {
 /*its really that simple dp inject is really just getting an external context and injecting it on instantantion*/
@@ -18,42 +17,14 @@ namespace AccountOwnerServer.Controllers
         }
 
         [HttpGet]
-        [Route("GetTicker/{ticker}/{country}")]
-        public async Task<string> GetTicker(string ticker, string country)
+        [Route("ticker/{ticker}")]
+        public async Task<string> GetTicker(string ticker)
         {
-
-            string url = $"https://api.agify.io?name={ticker}&country_id={country}";
+            string url = $"https://api.agify.io?name={ticker}";
             var response = await _httpClient.GetAsync(url);
-            Response.Headers.Add("Food","Pizza");
-            var code = Response.StatusCode;
-            Response.Headers.Add("fuckingawesomestatuscode",$"{code}");
-
-
             return await response.Content.ReadAsStringAsync();
         }
 
 
-        /*you must include both the route and the http method otherwise it will thow an exeption*/
-        [HttpGet]
-        [Route("hello")]
-        public string SayHello(){
-        return "Hello there darling" ;
-        }
-
-
-        [HttpGet]
-        public async Task<string> DoesItKnow(string food)
-        {
-            string testUrl = $"This is the test url for food {food}";
-            return testUrl;
-        }
-
-
-/*There is no need to specify the controller name since we are using a default*/
-        [Route("SayGoodbye")]
-        public ActionResult<string> SayGoodbye()
-        {
-            return "Toodaloo!";
-        }
     }
 }
--- stockbuysellbackendapi/stockbuysellbackendapi/obj/Debug/net8.0/stockbuysellbackendapi.AssemblyInfo.cs
+++ stockbuysellbackendapi/stockbuysellbackendapi/obj/Debug/net8.0/stockbuysellbackendapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("stockbuysellbackendapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d74ad1cc4787775365336bccf63afbeede306b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01c812e995c1c408b45d51e7f8dc840d50575ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("stockbuysellbackendapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("stockbuysellbackendapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
