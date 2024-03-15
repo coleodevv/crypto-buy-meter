1c02962956228fe84a3c3b915a8bf0e4e82928a8
coleodevv <cole@coleodevv.com>
2024-03-15 08:04:00 -0700

    changed to crypto picker 1day ema

--- cryptobuysellbackendapi/cryptobuysellbackendapi/obj/Debug/net8.0/cryptobuysellbackendapi.AssemblyInfo.cs
+++ cryptobuysellbackendapi/cryptobuysellbackendapi/obj/Debug/net8.0/cryptobuysellbackendapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cryptobuysellbackendapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a742ac35fb9c6991fa51793d8269e998a90aa23c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("cryptobuysellbackendapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cryptobuysellbackendapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
